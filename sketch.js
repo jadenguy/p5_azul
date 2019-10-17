8bb49850cf1c4af90dc992047ff03984e9dc1790
Alfredo Inostroza <jadenguy@gmail.com>
2019-10-17 19:23:20 -0400

    changed drawing orders and funcs

--- sketch.js
+++ sketch.js
@@ -1,40 +1,23 @@
 let mouseCounter = -1;
 let mouseCircle;
 const mouseAccept = 15;
+const bgColor = 100;
 const r = 10;
 function setup() {
   const minDim = min(windowWidth, windowHeight);
   createCanvas(minDim, minDim - 4);
   mouseCircle = createVector(0);
-  ellipseMode(RADIUS);
-  background(220);
-  colorMode(HSB, 1);
+  background(bgColor);
 }
 function windowResized() {
   const minDim = min(windowWidth, windowHeight);
   createCanvas(minDim, minDim - 4);
-  background(220);
+  background(bgColor);
 }
 function draw() {
-  background(220);
-  if (mouseIsPressed) {
-    mouseCounter++;
-    if (mouseCounter == 0) {
-      mouseCircle.set(mouseX, mouseY);
-    } else if (mouseCounter > mouseAccept) {
-      mouseCounter = mouseAccept;
-    }
-  }
-  else {
-    mouseCounter = -1;
-  }
-  const percent = clamp(mouseCounter / mouseAccept, 0, 1);
-  const zeroAngle = -HALF_PI;
-  stroke(color(.3, 1, map(percent, 0, 1, 0, .5), .5));
-  fill(color(.3, 1, map(percent, 0, 1, .5, 1), .5));
-  if (mouseCounter > 0) {
-    print(mouseCounter / mouseAccept);
-    arc(mouseCircle.x, mouseCircle.y, r, r, zeroAngle, zeroAngle + (percent * TWO_PI));
+  background(bgColor);
+  if (mouseCheck()) {
+    mouseDraw();
   }
 }
 function clamp(num, min, max) {
@@ -42,4 +25,35 @@ function clamp(num, min, max) {
 }
 function average(arr) {
   return arr.reduce((a, b) => a + b, 0) / arr.length
+}
+function mouseDraw() {
+  push();
+  ellipseMode(RADIUS);
+  colorMode(HSB, 1);
+  const percent = clamp(mouseCounter / mouseAccept, 0, 1);
+  const zeroAngle = -HALF_PI;
+  stroke(0);
+  fill(0, 1, 1, .5);
+  circle(mouseCircle.x, mouseCircle.y, r);
+  stroke(color(.3, 1, map(percent, 0, 1, 0, .5)));
+  fill(color(.3, 1, map(percent, 0, 1, .5, 1)));
+  print(mouseCounter / mouseAccept);
+  arc(mouseCircle.x, mouseCircle.y, r, r, zeroAngle, zeroAngle + (percent * TWO_PI));
+  pop();
+}
+function mouseCheck() {
+  if (mouseIsPressed) {
+    mouseCounter++;
+    if (mouseCounter == 1) {
+      mouseCircle.set(mouseX, mouseY);
+    }
+    else if (mouseCounter > mouseAccept) {
+      mouseCounter = mouseAccept;
+    }
+    return true;
+  }
+  else {
+    mouseCounter = 0;
+    return false;
+  }
 }
