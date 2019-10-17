1a111983b0b787bdbdfae3f8a4df71552085cb86
Alfredo Inostroza <jadenguy@gmail.com>
2019-10-17 19:11:02 -0400

    square board

--- sketch.js
+++ sketch.js
@@ -3,14 +3,16 @@ let mouseCircle;
 const mouseAccept = 15;
 const r = 10;
 function setup() {
-  createCanvas(windowWidth, windowHeight - 4);
+  const minDim = min(windowWidth, windowHeight);
+  createCanvas(minDim, minDim - 4);
   mouseCircle = createVector(0);
   ellipseMode(RADIUS);
   background(220);
   colorMode(HSB, 1);
 }
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight - 4);
+  const minDim = min(windowWidth, windowHeight);
+  createCanvas(minDim, minDim - 4);
   background(220);
 }
 function draw() {
@@ -19,18 +21,20 @@ function draw() {
     mouseCounter++;
     if (mouseCounter == 0) {
       mouseCircle.set(mouseX, mouseY);
+    } else if (mouseCounter > mouseAccept) {
+      mouseCounter = mouseAccept;
     }
   }
   else {
     mouseCounter = -1;
   }
   const percent = clamp(mouseCounter / mouseAccept, 0, 1);
-  const rotate = -HALF_PI;
-  stroke(color(.3, 1, map(percent, 0, 1, 0, .5),.5));
-  fill(color(.3, 1, map(percent, 0, 1, .5, 1),.5));
+  const zeroAngle = -HALF_PI;
+  stroke(color(.3, 1, map(percent, 0, 1, 0, .5), .5));
+  fill(color(.3, 1, map(percent, 0, 1, .5, 1), .5));
   if (mouseCounter > 0) {
     print(mouseCounter / mouseAccept);
-    arc(mouseCircle.x, mouseCircle.y, r, r, rotate, rotate + (percent * TWO_PI));
+    arc(mouseCircle.x, mouseCircle.y, r, r, zeroAngle, zeroAngle + (percent * TWO_PI));
   }
 }
 function clamp(num, min, max) {
