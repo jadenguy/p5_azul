6d6a9a7a38ac729e410425282b343442045881f3
Alfredo Inostroza <jadenguy@gmail.com>
2021-02-24 11:20:18 -0500

    added tilebag and input checker dependancy

--- tileBag.js
+++ tileBag.js
@@ -8,29 +8,6 @@ class Tile {
     constructor(colorIndex) {
         this.colorIndex = colorIndex;
     }
-    toString() {
-        let ret;
-        switch (this.colorIndex) {
-            case 0:
-                ret = RED;
-                break;
-            case 1:
-                ret = YELLOW;
-                break;
-            case 2:
-                ret = BLUE;
-                break;
-            case 3:
-                ret = BLACK;
-                break;
-            case 4:
-                ret = WHITE;
-                break;
-            default:
-                break;
-        }
-        return ret;
-    }
 }
 class Bag {
     constructor() {
